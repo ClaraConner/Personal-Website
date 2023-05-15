a2560ccc8cf2eb912a38c5bac2b43f3885b74989
Clara Conner <marie120902@gmail.com>
2023-05-14 21:10:53 -0500

    Header

---
+++ personalwebsite/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import Header from './components/Header.js'
+import './App.css';
+
+//fonts
+import './fonts/fleurondingbats-webfont.woff';
+import './fonts/fleurondingbats-webfont.woff2';
+import './fonts/fleuronmixed-webfont.woff';
+import './fonts/fleuronmixed-webfont.woff2';
+import './fonts/fleuronregular-webfont.woff';
+import './fonts/fleuronregular-webfont.woff2';
+
+function App() {
+  return (
+    <Header></Header>
+  );
+}
+
+export default App;
---
+++ personalwebsite/src/components/Header.js
@@ -0,0 +1,19 @@
+import "./Header.css"
+
+function Header() {
+    return (
+        <div className="Header">
+            <h className="C">C</h>
+            <div className="HeaderBtns">
+                {/* <button>About</button>
+                <button>Experience</button>
+                <button>Projects</button>
+                <button>Quick Links</button>
+                <button>Fun</button> */}
+            </div>
+        </div>
+      
+    );
+  }
+
+  export default Header;
